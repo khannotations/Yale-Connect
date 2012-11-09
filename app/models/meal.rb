103c35ca20281b53f88fe8ca48277d934cc87483
Akshay Nathan <akshay.nathan@yale.edu>
2012-11-09 15:05:39 -0500

    image storage

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,6 +5,7 @@ class Meal
   field :image, :default => "/assets/no_image.jpg"
   field :feedback
   field :done, :type => Boolean, :default => false
+  field :id, :type => String
 
   belongs_to :user_1, class_name: "User", foreign_key: "user_1_id"
   belongs_to :user_2, class_name: "User", foreign_key: "user_2_id"
